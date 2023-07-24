89e8c6403aa81165524854ad534998353778592f
Phlavio <77625202+Phlavio@users.noreply.github.com>
2023-07-24 18:59:06 +0800

    Merge remote-tracking branch 'origin/master'

--- src/components/Interests.vue
+++ src/components/Interests.vue
@@ -2,7 +2,7 @@
   <h1 class="title fixed-top inter">SOME OF <span class="outlinetext">MY INTERESTS</span></h1>
 <!--  expander-->
   <div class="options">
-    <div class="option active" style="--optionBackground: url(../assets/interest/biking.jpg); ">
+    <div class="option active" style="--optionBackground: url(src/assets/interest/biking.jpg); ">
       <div class="shadow"></div>
       <div class="label">
         <div class="icon">
@@ -13,7 +13,7 @@
         </div>
       </div>
     </div>
-    <div class="option" style="--optionBackground:url(https://lh3.googleusercontent.com/pw/AIL4fc9GQib0YMtq23zr-b_isl2fQQR0AAOXE_pcAh45Exj1-4gwy6OiV3_Yj6m7B-_K1m6Z1eEZZeESB9R6hpXH0zhI6nAdGqITXrDbN3sWD8DF7mL-8ry-MmklDxWUe5cZhC9eV8VPHVuRjDLhg7mVVO9W-5UttMLzOdF09ocx3uRgn23iF5ou4-AP4icZTnq1jnjX3UgsiCQe3AD2cP_XR987UlQUWl5QofGOrAkpD-ceNFY6OpNXX3cu5c5RqRNENDZQM_Tbwh6-M2FI40EakBJTBacdrJrLxMPElADxPOZoMTWG7DROm_x0hVkIUBhT0CDDkHHRyyUrWjrHSOINHPX2V-opLQbDj3szh8ZKdmiX4-0fcxmV8QeSZGfrh3nhwinTMJEr26mM4Mx5WEvuSgvjnY__AJssNK2lJf1fsFP-hMcsRNYDsdH_vEaGzBAI6mHgHPEZQ9iwhrvFJx_nEOnS_2EPtUMamTDZaNr9Or0A1vC1uNG8pAsCBRlU5EZrw-dzqAptxMa8zbpFt7eYbFN-kOZ8gx241sji4eN0W8mO-uuyDPv6ZfRW47tb7xvOHUAJgolWIi7CkloFhPeZvU3tH5UjL02T8ie441AUu1wDHxTS9l_vEjmBkU0n6x7GVwn9Q7QhbeefLvxyj01mGyjLkgwGRnDcfC7UU8NRwQEtzieQd8vw8y9saj2moPFAqqb2GiTZGSJMDFEKbM-xXFtVYEzx0RrjDM0EvpcRc0V8JmduDwbSZOJSk9kjAZInSk6mKAe7zpN13t-PJjyaPk2o0vWTn3jtN0IKndmdxK86ZaVgwO-8hdbnrLQIz7IJ6soTQegQx0AemDJsTsD4Al9gKuRpH20AyKeMfHczUuxYc2FJ3SjY3U5Y2dVDq6Ui76BaGcoe6jTMwsAP5veH8m9gDQfXUh1x94iWGO1P2kuWAZ5wUnW8spU-hhwZOg=w500-h667-s-no?authuser=2);">
+    <div class="option" style="--optionBackground:url(src/assets/interest/code.jpg);">
       <div class="shadow"></div>
       <div class="label">
         <div class="icon">
@@ -24,7 +24,7 @@
         </div>
       </div>
     </div>
-    <div class="option" style="--optionBackground:url(https://lh3.googleusercontent.com/pw/AIL4fc_BBTtJUCw8LM5a1mMpWJn5LtZlpsk3Yq0ifpBrbVBs2tNGYITrdWeQInxqLW24V9IGpFD7UBBTAEP3cSqvheyNVvJy4AJVcaFcwDKJe3OPmFqckeA5vEi8ryVTaNbiDZVcwthB_1Fuuyb6Dy_zQmkWVm6Iyf2KFyiOsYB6EeIDG1U2fpODR84Uv4CipwTz2FV-LaGCp6DN3xGELQjsk1DINjze_00VBXKfVQQu5Ip1PFumflDywVIcu009lGmlD8r8Qz731Yj0TswxV0dwN2OYFAzDP9sKB_YowGUReovmxBU1u8HLAYYTBJz3iWRtpJApp7TPVLt7CzueQkPEfXpjtJ5WQGuM6t9HrX8c6rGXVZ4wNfvlllM_9AdwzbfrqOXOxGobArpzCma82RH5FEi5qXdIFTJEHiBD_oleiqaoo4cnO8kTRNp6wx-E6e7-9n36VoWcmx8d98UIjR2J6GJV50wma3YYOt8tmW7DSbVMEtizxL8ee0yVlqCpaaK1abYiIKZ7ok-kME9Lg89_5msoMLw-CpMNhknROw4Yg9pfmZpmSPoAOrrZCec3q2LoVCHsaUL2xF49UQVJF49LqdRxSryx7EaErZ5E8F44YttH7h5hSWlibmD9LcyxLW_aqxRx2sYCeBqPhCAM16UElsfqoTtNesbZmeR8LXVYlGUZp7TK2lMdBI6HsXmirwhAZhGKnRU9IeOd69iydg8AzIRUbrQFgd_n7eRqoWS5nldpO87NtzS8Qwxt55xWjFjhRX0bMdog6IZpE7auJpKD5DNAO5sGEL9oLtQxbXGBV_giqMp0uMNZO-neyGpZR_5qGzo22YPZTMzFZJ21noBfRSLETYdCcTtZKyuVnrbwLyOH-7qd1ZP_ePlou1_RANAn2Do8Q5foWkpbvf9DQDUsyXav95cjBWiSGuVi6vJ_r_Zw8SnFkmfwlnhQtNrfjA=w500-h667-s-no?authuser=2);">
+    <div class="option" style="--optionBackground:url(src/assets/interest/gaming.jpg);">
       <div class="shadow"></div>
       <div class="label">
         <div class="icon">
@@ -35,7 +35,7 @@
         </div>
       </div>
     </div>
-    <div class="option" style="--optionBackground:url(https://lh3.googleusercontent.com/pw/AIL4fc8UyIrlr6-0cAwfxcqkG18pYzW4H3gUcjyeQBjXZQLMu25OE1sG8rbyfHYzzTO6tvFnXxkyrbT9BEPhfRV0-Drv4ufNdQienjL5vjsfe8kJf9Vxi-cTM2IZwK_q4Gdw42tg_7uqHZD99FckNYPE7WGUkEQBjAd7Mc4mhrqXTCpquMPKCbZrRVPVq8_bQmPrdpUdnKqr8PNJhHiB8SAX6cMISm_G6h8lxQqRFsYQ6L9BNjEBvWrBHKBxt8HoXnQg4Vpx687CDkgulacQmWNeKwwzYrCxPfBpb5cGSo473NBDBJbH-dHCTRkDU-lhHWyG1TFhVMmjTMdmJK4XBGY2hO9EueoO0c73VmLhADzSclh7-pMcINjCfQS8ZTK2B7j4pJdUnFMYkPJV1MAEPsMR0ezj0vKMKxnlzY6RjGJa2oCelKIX_s6mPYwMLANWXr4_pTQeCsaJFTlzOpd1uuYaHLierm29T57CYxKO46WK0fiGJpBNYvB_gQvwFZ89-9Ab2JlSmbaxj874FjeOMyz_JcfA0leNeAeF_LMYWx_jIGdJ42W0CQEOcENZafwgeWfCO-Kq-XsaOyFiSrKF-D7euDwNFfvClZq0OI4C3BdP91GT9HFYWjNVcfpFf6gW7pOyjKq-U8uFtNsXYIRUS-EFCHoucwnCuTFXfVR7--4ybRrLt-7wDPaTh2zkpA-Rm3SDfe7Wmnc5HdZiSZGj0rySBaU948IvmCtWPyY-OjpwVP9WaYUq2iuenzhwjpLzTXkggeaJSkjpcIAbHqdP4BV9uu6HtNgy5uHrYOLgKL7NtoxKZHC93H_vOmoePMM7lQJcDu3CPiG_8tSRpLu-6-E_jrqAV2LIUDmy6uva56jC9xCsdHEnYAR9p4wZigexGxh207-D0wrboQcvGONUazkmBH354bZc0IHkuI75Qm9yvFoolAl1vtV-_61inSb7=w500-h667-s-no?authuser=2);">
+    <div class="option" style="--optionBackground:url(src/assets/interest/offroading.jpg);">
       <div class="shadow"></div>
       <div class="label">
         <div class="icon">
@@ -46,7 +46,7 @@
         </div>
       </div>
     </div>
-    <div class="option" style="--optionBackground:url(https://lh3.googleusercontent.com/pw/AIL4fc9ZPiB8v92RDFvXRLJq7krSzvm8b2pHMli-3Nx6-Wkgr_3gX37b-f_Jf1OZ8w0BgNQoiejPcv8fNTcKxd1iEnzA6D41HIwB8pjtmrJrvvxemjc4m62BqQS7Ecrj5W6lf6lvE5u8IGWVb5AETmbylg00YwNNJgiXOffrKxNaCBp3bib9oPdiWoOPS9mU2ZDzZqWZQ9zBegmlHMI9kJI0orTgMdDIcgSG-o_tFiA6l5L_cISN--PTjnELc0xp20hMlbWMLJX0f4K-4MOS2btpJe1S69zJHW8EwsX_6AaDPoxoLdL2o-oTK5FIqcc-XUuD78lKxz4LRwtBUrvORdO9JOYCfpC2eH_fJmW3GgvlrrNGoGIb354T_fanw14Hx5kMWplAaaV_6q63Nn9kuIp0DABsedwwBXR0sV_5Iul6VG0spbG6mnNjWBYBM3RXIjNISVQ4pfax1_zt7Yly6qNYkv1JOFGKxfPIZio1XxxCxFF30lwwgA4lLfswu2W8ezTRSy8FX7QotHPQwncRgDE80CY-xNam1qT3k4VsQ6ULh2OGZqiDH4-GcuWCeBKpCiwsBOHfy7OrfQ6KJPNrHZTKWPrJGGOWks33ivbgXIzUWlZG-B6DQPUsX2YvSzJvAYQgmWMFdSgjSWfzB5V7vR9gqeFjCZN38TRCTaFWq6uljZRZO6JSnL27INOgoAfGBSsFjwRqk25UvoYokbhfvQfmV7_81AhZOVfJMnNDPRrst6cSWQFJuB4Dfv5ymjDbfKCHlSV3IpiVCBImqfiUryX6dLjOYJUzWsY2XfVgj-XgDSjCTC6gpplGWzk4CT0qt1ZbtXdjw7MwREg6Tc20BpUtZqNJh6hQmg_mUDNrFEexsOELKoFmvaz6coWdeZHxMCwsit1HZ8bwWGqcGmQSfc1v7AnPPq1qHM_RpzQBLL-1sXt95N1iafzvxNb6zwsF=w500-h667-s-no?authuser=2);">
+    <div class="option" style="--optionBackground:url(src/assets/interest/music.jpg);">
       <div class="shadow"></div>
       <div class="label">
         <div class="icon">
