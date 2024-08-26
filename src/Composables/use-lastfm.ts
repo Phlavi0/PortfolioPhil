99934970ceca6fd3615717a29aeb1cfcef24be1b
philipcadungog <philip.cadungog@3clicksapparel.com>
2024-08-26 21:24:47 +0800

    last fm loop

--- src/Composables/use-lastfm.ts
+++ src/Composables/use-lastfm.ts
@@ -1,21 +1,35 @@
 import type {recentlisten} from "./types";
-import { ref, onMounted } from 'vue';
+import { ref, onMounted, onUnmounted } from 'vue';
 
 const API_URL = 'https://ws.audioscrobbler.com/2.0/?';
 const params = new URLSearchParams({method:'user.getrecenttracks', user:'phlavi0', api_key: '4e94a5a7ca331cdc19c4f269b0abecc0',totalpages:'10', format:'json'});
 const tracks = ref<recentlisten[]>([]);
 export function useLastfm(){
-    onMounted(async () => {
-        try {
-            const response = await fetch(API_URL + params)
+    let intervalId = null;
+    onMounted(() => {
+        const fetchData = async () => {
+          try {
+            const response = await fetch(API_URL + params);
             const data = await response.json();
             tracks.value = data.recenttracks.track;
-
-        } catch (error) {
-            console.error(error)
+          } catch (error) {
+            console.error(error);
+          }
+        };
+      
+        // Fetch data immediately when mounted
+        fetchData();
+      
+        // Set up an interval to fetch data every 5 seconds
+        intervalId = setInterval(fetchData, 5000);
+      });
+      
+      onUnmounted(() => {
+        // Clear the interval when the component is unmounted to prevent memory leaks
+        if (intervalId) {
+          clearInterval(intervalId);
         }
-        setInterval(useLastfm, 5000);
-    });
+      });
 
     return {tracks}
 }
